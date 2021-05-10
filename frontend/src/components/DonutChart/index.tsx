5818d5b01336bc0be03d4f324f6040ed3eeb0ecd
Allcb <allancbarbosa@gmail.com>
2021-05-09 21:16:23 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -15,19 +15,14 @@ function DonutChart() {
 
     useEffect(() => {
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
-            .then(Response => {
-                const data = Response.data as SaleSum[];
+            .then(response => {
+                const data = response.data as SaleSum[];
                 const myLabels = data.map(x => x.sellerName);
                 const mySeries = data.map(x => x.sum);
 
                 setChartData({ labels: myLabels, series: mySeries });
             });
     }, []);
-
-    // const mockData = {
-    //     series: [477138, 499928, 444867, 220426, 473088],
-    //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    // }
 
     const options = {
         legend: {
